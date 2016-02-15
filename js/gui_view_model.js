158edd84db5cbff9e5df2c0aebc5bbe54d46f21c
developer2020 <dev276236@gmail.com>
2016-02-15 23:34:46 +0400

    more refactoring - cleaner interface between gui view and controller

--- js/gui_view_model.js
+++ js/gui_view_model.js
@@ -1,17 +1,63 @@
+/*gui view interface with controller: 
+- gui view provides currentl syelected item to controller to bounce it on the map 
+(contoller method set_filtered_item)
+- done for step 1, call to set_filtered_item
+
+- gui view provides final selection at beginning of each step
+(contrioller should update 
+
+- gui view at step 1 requests items to display . On this request controller also updates map
+- gui view at step 2 requests items to display and provides finally selcted location (process_step_update)
+ On this request controller also updates map*/
 var GUIViewModel = function(update_map, controller){
 	//var self = this;
     this.controller = controller; 
 	this.map_updater = update_map; 
-	this.current_filter_list = ko.observableArray();
-	this.current_step = ko.observable(0); 
-	this.selected_location = ko.observable("");
-	this.cityName = ko.observable("")
-	this.step_list = Array()
-	this.stepTitle = ko.computed(function() {
 
-    return ("Step " + String(this.current_step()));
+	this.current_filter_list = ko.observableArray();
+	this.current_step = ko.observable(1);
+
+	this.filtered_location_name = ko.observable("");
+
+	this.filtered_location = {}; 
+
+
+	this.selected_source = ko.observable({});
+	this.selected_destination = ko.observable({});
+
+	this.cityName = ko.observable("")
+	
+	this.step_list = Array()
+	this.messages  = {
+		STEP1_AWAITING_INPUT: " : please select starting point",
+		STEP1_NO_SOURCE_SELECTED: " : no starting point selected",
+		STEP1_SOURCE_SELECTED: ": please click next step to continue",  
+		STEP2_AWAITING_INPUT: "please select destination",
+		STEP2_NO_DESTINATION_SELECTED: " : no destination selected",
+		STEP2_DESTINATION_SELECTED: ": please click next step to continue"
+	}
+
+    this.step_msg = ko.observable(this.messages.STEP1_AWAITING_INPUT);
+
+	this.step_and_status = ko.computed(function() {
+	var msg = ""
+    return ("Step " + String(this.current_step()) + this.step_msg());
   }, this);
-   
+
+	this.selected_source_destination_display = ko.computed(function() {
+	
+	if ($.isEmptyObject(this.selected_source())){
+		return("");
+	}
+
+	if ($.isEmptyObject(this.selected_destination())){
+		return("From : "  + this.selected_source().name);
+	}
+
+	return ("From : "  + this.selected_source().name  + " to : " + this.selected_destination().name);
+  }, this);
+
+   this.update_current_filter_list(this.controller.get_filtered_list_for_current_step(this.current_step()));
 }
 
 GUIViewModel.prototype.update_current_filter_list = function(new_list){
@@ -22,28 +68,60 @@ GUIViewModel.prototype.update_current_filter_list = function(new_list){
 	    }
 }
 GUIViewModel.prototype.next_step = function(){
-	this.controller.next_step(); 
+    if (this.current_step()===1){
+	  if (!$.isEmptyObject(this.filtered_location)){
+	  	this.current_step(2);
+	  	this.selected_source(this.filtered_location);
+	  	this.step_msg(this.messages.STEP2_AWAITING_INPUT);
+	  	this.controller.process_step_update();
+	  	this.update_current_filter_list(this.controller.get_filtered_list_for_current_step(this.current_step()))
+	  }
+	  else{
+	  	this.step_msg(this.messages.STEP1_NO_SOURCE_SELECTED);
+	  }
+    }
+	//this.controller.next_step(); 
 }
 
 GUIViewModel.prototype.previous_step = function(){
-	this.controller.previous_step(); 
+
 }
 
 
 GUIViewModel.prototype.apply_filter = function() {
-
-    var location = this.selected_location();
-    if (location === "") {
+    console.log(this.filtered_location_name())
+    //console.log(this.filtered_location().name)
+    if (this.filtered_location_name() === "") {
         this.reset_filter();
     } else {
+    	var location = this.filtered_location_name(); 
         filter_out = function(item) {
             return item.name != location;
         }
         this.current_filter_list.remove(filter_out);
+    }
+    console.log(this.current_filter_list().length);
+    if (this.current_filter_list().length===1){
+    
+    	if(this.current_step()===1){
+   
+    		this.filtered_location = this.current_filter_list()[0];
+    		this.controller.set_filtered_item(this.filtered_location); 
+    		console.log(this.filtered_location);
+    	}
+    	
     }
 }
 
 GUIViewModel.prototype.reset_filter = function(){
 		this.update_current_filter_list(this.step_list);
         this.map_updater.update_map(); 
+        if (this.current_step()===1){
+        	this.step_msg(this.messages.STEP1_AWAITING_INPUT);
+        	this.filtered_location("");
+		    this.selected_source = "";
+			this.selected_destination ="";
+        }
+
+
 }
--- js/main.js
+++ js/main.js
@@ -15,109 +15,65 @@
 //is not supposed to be accessed by any object other than data model.
 //========================================================================================
 //Global variables (plan to  make them configurable too)
-var MAP_INITIAL_POS = {lat: 24.4667, lng: 54.3667};
-var CITY_NAME       = "ABU DHABI"
+var MAP_INITIAL_POS = { lat: 24.4667, lng: 54.3667 }; // do not like the deaulf, need to customize 
+var CITY_NAME = "ABU DHABI"
 
-var sources_list = Array(); 
-var destinations_list = Array();
-var bus_route_list = Array(); 
-
-
-
-var map_handler; 
-
+var map_handler;
 //==========================
-var UpdateMap = function(){
-	var self = this; 
-	self.update_map = function(){
-		console.log("map was just updated");
-	}
-}
-
-//============================
-var Controller = function(){
-  var self = this; 
-  this.data_model = new DataModel(bus_routes, bus_stops, map_objects, 2000);
-  var update_map = new UpdateMap();
-  this.gui_view = new GUIViewModel(update_map, self); 
-  this.gui_view.cityName(CITY_NAME);
-  this.gui_view.update_current_filter_list(sources_list);
-  this.gui_view.current_step(1);
-  ko.applyBindings(this.gui_view);
-
-  //document.getElementById("nextStepButton").addEventListener("click", this.next_step);
-  //document.getElementById("previousStepButton").addEventListener("click", this.previous_step);
-}
-
-Controller.prototype.previous_step = function(){
-	if (this.gui_view.current_step()===2){
-	  this.gui_view.update_current_filter_list(sources_list);
-	  this.gui_view.current_step(1);
+var UpdateMap = function() {
+    var self = this;
+    self.update_map = function() {
+        console.log("map was just updated");
     }
 }
 
-Controller.prototype.next_step = function(){
-	if (this.gui_view.current_step()===1){
-	  this.gui_view.update_current_filter_list(destinations_list);
-	  this.gui_view.current_step(2);
-    }
-    //console.log(local_distance_matrix);
-}
-
 //============================
+var Controller = function() {
+    var self = this;
+    this.data_model = new DataModel(bus_routes, bus_stops, map_objects, 2000);
+    var update_map = new UpdateMap();
+    this.gui_view = new GUIViewModel(update_map, self);
+    this.gui_view.cityName(CITY_NAME);
+    //this.gui_view.update_current_filter_list(this.data_model.get_map_objects({class: "community"}));
 
-function initialize(){
-  //populate current list with communites 
-  for (var i =0; i<map_objects.length; i++){
-  	//console.log(map_objects[i].class);
-  	if (map_objects[i].class=="community"){
-  		sources_list.push(map_objects[i]);
-  	}
-  	else
-  	{
-  		destinations_list.push(map_objects[i]);
-  	}
-  }
+    ko.applyBindings(this.gui_view);
 
-  for (var i = 0; i<bus_routes.length; i++){
-  	 if (bus_route_list.indexOf(bus_routes.route)===-1){
-  	 	bus_route_list.push(bus_routes.route);
-  	 	//STOPPED HERE UNTESTED
-  	 }
-  }
-
-  //console.log(gui_view);
-
-  //////////////
-  
-
-  //////////
- 
-
-
-  
-
- 
-  this.current_step = 1;
-  //ko.applyBindings(stepTitle, document.getElementById('steps'));
-  /*
-   
-*/
+    //document.getElementById("nextStepButton").addEventListener("click", this.next_step);
+    //document.getElementById("previousStepButton").addEventListener("click", this.previous_step);
 }
 
-function initMap(){
-	debug_main();
-	map_handler = new MapHandler(MAP_INITIAL_POS);
-	map_handler.add_locations(bus_stops);
+//console.log(local_distance_matrix);
+
+//=============================
+//interface with gui view 
+Controller.prototype.get_filtered_list_for_current_step = function(step) {
+    if (step === 1) {
+        return (this.data_model.get_map_objects({ class: "community" }));
+    }
+    //for step 2, need to implement list of reacheable stops in data model 
+}
+Controller.prototype.set_filtered_item = function(item) {
+
+}
+Controller.prototype.set_filtered_source = function(source) {
+
+}
+
+Controller.prototype.process_step_update = function(source) {
+
+}
+
+
+function initMap() {
+    debug_main();
+    map_handler = new MapHandler(MAP_INITIAL_POS);
+    map_handler.add_locations(bus_stops);
 
 }
 
 
 
 
-(function main(){
- //debugOut(); 
-  initialize(); 
-  var controller = new Controller();
-})(); 
-
+(function main() {
+    var controller = new Controller();
+})();
